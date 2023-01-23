d31b5b421953498ac9415d9f077ad898645b28a0
sagm3012 <asadbeksobirov2002@gmail.com>
2023-01-23 17:49:14 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using KESCHA.Classes;
+
+Console.Write("Enter your name: ");
+string name  = Console.ReadLine();
+string greeting = $"Hello {name}";
+Console.WriteLine(greeting);
+
+Console.Write("Enter your age: ");
+string ageAsString = Console.ReadLine();
+Console.WriteLine("Converting..... ");
+int age = int.Parse(ageAsString);
+Console.WriteLine($"Successfully converted! {age}");
+int keschasAge = 3;
+int ageDifference = age - keschasAge;
+Console.WriteLine($"The differenc between your and Kescha's age is {ageDifference}");
+
+
+
+
+Animal animal = new Animal();
+animal.ID = 1;
+Console.WriteLine($"Animal Id number is: {animal.ID}");
+animal.Name = "Kescha";
+Console.WriteLine($"Animal name is: {animal.Name}");
